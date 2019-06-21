07abc0e767932798386af0c7a6aec0d021f38534
DAKER1994 <50975973+DAKER1994@users.noreply.github.com>
2019-06-21 20:33:04 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = "WchSpider",
+    version = "1.0.0",
+    description = "VERY GOOD",
+    author = "myself and I",
+    author_email = "email@someplace.com",
+    url = "whatever",
+    packages = ['aiohttp','lxml'],
+    long_description = """you can callback"""    
+ ) 
